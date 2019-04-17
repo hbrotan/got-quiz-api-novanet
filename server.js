f0c00a1ae3bec79a3862c1293b1950c17a5a7478
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-17 09:02:33 +0200

    fix sql

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ router.get('/', function(req, res) {
 router.route('/answers')	
 	.get(function(req, res){
 		sql.execute({  
-			query: 'SELECT [Name],[Q1],[Q2],[Q3],[Q4],[Q5],[Q6],[Q7],[Q8],[Q9],[Q10],[Q11],[Q12],[Q13],[Q14],[Q15],[Q16],[Q17],[Q18],[Q19],[Q20],[Q21],[Q22],[Q23],[Q24],[Q25],[Q26],[Q27],[Q28],[Q29],[Q30] FROM dbo.GotQuizNrk X WHERE X.Id = (SELECT MAX(Id) FROM dbo.GotQuizNovanet WHERE Email = X.Email) ORDER BY [Name]'
+			query: 'SELECT [Name],[Q1],[Q2],[Q3],[Q4],[Q5],[Q6],[Q7],[Q8],[Q9],[Q10],[Q11],[Q12],[Q13],[Q14],[Q15],[Q16],[Q17],[Q18],[Q19],[Q20],[Q21],[Q22],[Q23],[Q24],[Q25],[Q26],[Q27],[Q28],[Q29],[Q30] FROM dbo.GotQuizNovanet X WHERE X.Id = (SELECT MAX(Id) FROM dbo.GotQuizNovanet WHERE Email = X.Email) ORDER BY [Name]'
 		}).then( function( results ) {
 			res.send(results);
 		}).catch(function(error){
