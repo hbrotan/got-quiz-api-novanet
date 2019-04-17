12ea8b37d437b453cdf91d2b073fbc3f81325d99
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-17 08:50:56 +0200

    feat get answers

--- server.js
+++ server.js
@@ -47,6 +47,16 @@ router.get('/', function(req, res) {
 });	
 	
 router.route('/answers')	
+	.get(function(req, res){
+		sql.execute({  
+			query: 'SELECT [Name],[Q1],[Q2],[Q3],[Q4],[Q5],[Q6],[Q7],[Q8],[Q9],[Q10],[Q11],[Q12],[Q13],[Q14],[Q15],[Q16],[Q17],[Q18],[Q19],[Q20],[Q21],[Q22],[Q23],[Q24],[Q25],[Q26],[Q27],[Q28],[Q29],[Q30] FROM dbo.GotQuizNrk X WHERE X.Id = (SELECT MAX(Id) FROM dbo.GotQuizNrk WHERE Email = X.Email) ORDER BY [Name]'
+		}).then( function( results ) {
+			res.send(results);
+		}).catch(function(error){
+			res.json(error);
+			throw error;
+		});	
+	})
 	.post(function(req, res){
         // Add answer			
 		
