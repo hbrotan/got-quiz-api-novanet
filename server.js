c7520bcc4d2766fc4da2ec3c90e41c42d916a01e
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-24 18:39:21 +0200

    api for characters

--- server.js
+++ server.js
@@ -45,6 +45,18 @@ var router = express.Router();
 router.get('/', function(req, res) {
     res.json({ message: req.headers['x-forwarded-for'] || req.connection.remoteAddress });   
 });	
+
+router.route('/characters')	
+	.get(function(req, res){
+		sql.execute({  
+			query: 'SELECT [Name], [Status] FROM dbo.GotQuizCharacters'
+		}).then( function( results ) {
+			res.send(results);
+		}).catch(function(error){
+			res.json(error);
+			throw error;
+		});	
+	})		
 	
 router.route('/answers')	
 	.get(function(req, res){
